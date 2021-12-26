21f0880f5606be0c91e99882fe669285e3bfc775
ebrahim78652 <ebrahimkarjatwala@gmail.com>
2021-12-26 22:49:28 +0100

    things implemented: 1) FahrtDetails part okay

--- carSharer/src/main/java/de/unidue/inf/is/FahrtDetailsServlet.java
+++ carSharer/src/main/java/de/unidue/inf/is/FahrtDetailsServlet.java
@@ -1,15 +1,27 @@
 package de.unidue.inf.is;
 
+import de.unidue.inf.is.domain.Fahrt;
+import de.unidue.inf.is.stores.FahrtStore;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class FahrtDetailsServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        System.out.println(req.getParameter("fid"));
+        int fahrtId= Integer.parseInt(req.getParameter("fid"));
+        try(FahrtStore fahrtStore = new FahrtStore()){
+            List<Fahrt> trip= fahrtStore.getAllInfoForTrip(fahrtId);
+            req.setAttribute("trip", trip);
+            req.getRequestDispatcher("/fahrt_details.ftl").forward(req, resp);
+
+        }
     }
 }
